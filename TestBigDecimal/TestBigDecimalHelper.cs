20741fb12db5937c88533ebf0ce4c7c257f41f31
AdamWhite <adamwhitehat@outlook.com>
2022-04-18 03:33:18 -0600

    Fix test project. Add failing test cases from issue.

---
+++ TestBigDecimal/TestBigDecimalHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace TestBigDecimal
+{
+    using System;
+    using System.Text;
+    using ExtendedNumerics;
+    using ExtendedNumerics.Helpers;
+    using NUnit.Framework;
+
+    public static class TestBigDecimalHelper
+    {
+        public static String GetInternalValues( BigDecimal bigDecimal )
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine( $"{bigDecimal.ToString()}" );
+            result.AppendLine( $"Mantessa: {bigDecimal.Mantissa}" );
+            result.AppendLine( $"Exponent: {bigDecimal.Exponent}" );
+            result.AppendLine( $"DecimalIndex: { bigDecimal.GetDecimalIndex()}" );
+            result.AppendLine( $"Length: {bigDecimal.Length}" );
+            result.AppendLine( $"SignifigantDigits: {bigDecimal.SignifigantDigits}" );
+
+            return result.ToString();
+        }
+    }
+}
