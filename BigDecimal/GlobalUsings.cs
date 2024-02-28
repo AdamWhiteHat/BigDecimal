b2e9ed6849af5e3ba5349798f8bb360da0ac750f
Protiguous <Protiguous@Protiguous.com>
2024-02-27 18:33:40 -0700

    Some global usings and defines.
    I'm not sure when the defines are compiled, or if we'll still need each .cs file to just define them again.

---
+++ BigDecimal/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// BigDecimal is copyright by Adam White.
+// MIT License.
+// https://github.com/AdamWhiteHat/BigDecimal
+
+global using System;
+global using System.Diagnostics;
+global using System.Linq;
+global using System.Numerics;
+global using System.Runtime.CompilerServices;
+global using System.Text;
